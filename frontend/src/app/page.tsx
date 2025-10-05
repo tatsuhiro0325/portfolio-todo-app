82f6f354312838289a5c091f854730941540438a
Tatsuhiro <2212172e@cloud.kobe-u.jp>
2025-10-05 16:50:34 +0900

    プルリクの作成

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
     createTodo(data)
   }
 
+  
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <div className="bg-white p-8 rounded-lg shadow-md max-w-md mx-auto my-20 mt-20">
